8546f66df499a127d8b69ce800fb1a24691eb6af
Kewen Belmont <141522522+Shellbaka@users.noreply.github.com>
2024-01-10 18:21:22 -0300

    Meu código c# de um Celular.

---
+++ trilha-net-poo-desafio_Celular_Kewen-main/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Nokia:");
+
+Smartphone nokia = new Nokia(numero: "81999999999", modelo: "Dragão Voador", imei: "11123356588869855", memoria: 32);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Duolingo");
+nokia.MostrarInformacoesAparelho();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular iPhone");
+
+Smartphone iphone = new Iphone(numero: "81996699669", modelo: "Universo Cósmico", imei: "68974563211323464", memoria: 16);
+iphone.Ligar();
+iphone.InstalarAplicativo("Mimo");
+iphone.MostrarInformacoesAparelho();
